aab59b67aa1cb8f71d41b3297594b34b77dada20
JamalAlMahrooqi <130425371+JamalAlMahrooqi@users.noreply.github.com>
2023-05-10 12:01:27 +0400

    Add files via upload

---
+++ Fahrenheit to Celcius.py
@@ -0,0 +1,9 @@
+Value=int(input("Enter a value: "))
+ans=input("Is the value you entered in celcius of fahrenheit (answer with c/f)")
+if ans=="c":
+    convert=Value*33.8
+    print(str(convert) +" Fahrenheit")
+else:
+    if ans=="f":
+        convert=Value/33.8
+        print(str(convert) +" Celcius")
