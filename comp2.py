07232c496450c4fbb76d4b9df63c5c629fc372a5
Jamal Al Mahrooqi <130425371+JamalAlMahrooqi@users.noreply.github.com>
2023-05-15 11:29:33 +0400

    Add files via upload

---
+++ comp2.py
@@ -0,0 +1,37 @@
+# def sum_digits(x):
+#     sum=0
+#     r=str(x)
+#     for i in range(len(r)):
+#        sum= sum+ int(r[i])
+#     return sum
+#     
+#     
+# result=sum_digits(234)
+# print(result)
+#
+
+
+
+# import math as m
+# 
+# def poly_area(n,side):
+#     area=(n * side**2) / (4 * m.tan(m.pi / n))
+#     return area
+#                  
+# 
+# print(poly_area(5,6.5))
+
+
+
+
+# vowels="aeiouAEIOU"
+# word=input("Give me a word: ")
+# 
+# def countVowels(string):
+#     count=0
+#     for i in range(len(string)):
+#         if string[i] in vowels:
+#             count+=1
+#     return count
+# 
+# print(countVowels(word))
