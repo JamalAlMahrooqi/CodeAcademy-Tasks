eb7e574ec4709a41712fc1b6997f1c6adb1ef3a5
Jamal Al Mahrooqi <130425371+JamalAlMahrooqi@users.noreply.github.com>
2023-07-03 15:59:34 +0400

    Delete Files.py

--- Files.py
+++
@@ -1,8 +0,0 @@
-memory=input("Enter your computer memory: ")
-file_size=input("Enter a file size: ")
-number_of_files=input("Enter the number of files: ")
-
-if(int(file_size)*int(number_of_files)<=int(memory)):
-    print("The file can be stored")
-else:
-    print("The file can't be stored")
